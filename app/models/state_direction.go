15fa81190c168de2585cb780662452d07122d373
owen <youwen21@yeah.net>
2021-06-17 15:44:46 +0800

    优化

--- app/models/state_direction.go
+++ app/models/state_direction.go
@@ -1,13 +1,13 @@
 package models
 
 type StateDirection struct {
-	StateClassId     int      `json:"state_class_id"`
-	StateDirectionId int      `json:"state_direction_id"`
-	StateFrom        string   `json:"state_from"`
-	StateTo          string   `json:"state_to"`
+	StateClassId     int      `form:"state_class_id" json:"state_class_id"`
+	StateDirectionId int      `form:"state_direction_id" json:"state_direction_id"`
+	StateFrom        string   `form:"state_from" json:"state_from"`
+	StateTo          string   `form:"state_to" json:"state_to"`
 	Label            string   `form:"label" json:"label"`
-	IsDeleted        int      `json:"is_deleted"`
-	DeletedAt        DateTime `json:"deleted_at"`
-	CreatedAt        DateTime `json:"created_at"`
-	UpdatedAt        DateTime `json:"updated_at"`
+	IsDeleted        int      `form:"is_deleted" json:"is_deleted"`
+	DeletedAt        DateTime `form:"deleted_at" json:"deleted_at"`
+	CreatedAt        DateTime `form:"created_at" json:"created_at"`
+	UpdatedAt        DateTime `form:"updated_at" json:"updated_at"`
 }
